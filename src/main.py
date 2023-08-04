d367d8393e8ad206b1bc3aea49cf87c3250946e5
Ghost <OverlyGenericAddress@pm.me>
2023-08-04 21:52:54 +0200

    Implemented basic messenger UI
    Capabilities:
    - User can input messages
    - Messenger can display messages
    
    Known flaws:
    - Session state does not display messages before rerunning the script
    - Send button doesn’t work yet

--- src/main.py
+++ src/main.py
@@ -1,43 +1,56 @@
-# Description: Main file for running the chat app
-# Run with: streamlit run src\main.py
+"""
+Main file for running the chat app
+Run with streamlit run src\main.py
 
+https://pypi.org/project/streamlit-chat/
+"""
 import time
+import datetime
 import streamlit as st
 from streamlit_chat import message
 
 
+# Add a message to the chat history
+def add_message(string_value: str,
+                true_if_user: bool = True,
+                time_stamp: datetime.datetime = datetime.datetime.now()):
+    # Add message dict to session state
+    st.session_state.history.append({'true_if_user': true_if_user,
+                                     'message': string_value,
+                                     'sent': time_stamp})
+
+
+def send_message():
+
+    # Add user message to chat history and rerun
+    add_message(st.session_state.message, true_if_user=True)
+
+    # Show a spinner during the api call
+    with st.spinner(text='Sending message...'):
+        # Add API call here!
+        time.sleep(1)
+        add_message("This works!", False)
+
 if __name__ == "__main__":
 
-    # Set page title
+    # Set page title and containers
     st.title("Simple Chat App")
+    message_container = st.empty()
+    input_container = st.empty()
 
-    # Initialize chat history if it doesn't exist and display it if it does
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    # Initialize session state and display messages if they exist
+    if "history" not in st.session_state:
+        st.session_state.history = []
     else:
-        for i in range(len(st.session_state['messages'])):
-            message(st.session_state['messages'][i]['content'],
-                    is_user=st.session_state['messages'][i]['role'] == 'user',
-                    key=f"{i}_user")
+        with message_container.container():
+            for i in range(len(st.session_state['history'])):
+                message(message=st.session_state['history'][i]['message'],
+                        is_user=st.session_state['history'][i]['true_if_user'],
+                        key=str(i))
 
     # Handle user input
-    if prompt := st.text_input("Type your message here..."):
-
-        # Send user message to API
-        message_id = 201
-
-        # Handle API response
-        if message_id == 201:
-            with st.spinner(text='Generating response...'):
-                # Get API response using message ID
-                answer_content = "Great this works!"
-                time.sleep(2)
-
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
-
-            # Add API response to chat history
-            st.session_state.messages.append(
-                {"role": "assistant", "content": answer_content})
-        else:
-            st.error("Something went wrong...")
+    with input_container.container():
+        st.session_state.message = st.text_input("Type your message here...",
+                                                 on_change=add_message,
+                                                 key="message_input")
+        st.button("Send", on_click=send_message)
