23460007d5fa7fe9aacfaaf5925825256bd72658
yagubi <rachid.yagubi@adesso.de>
2023-08-08 00:44:15 +0200

    added backend connection

--- src/main.py
+++ src/main.py
@@ -7,9 +7,11 @@ https://pypi.org/project/streamlit-chat/
 import streamlit as st
 import interface as msgr
 from interface import init
+import requests
+import json
 
 if __name__ == "__main__":
-
+    session = requests.Session()
     # Initialize layout and containers
     init.layout()
     message_container = st.container()
@@ -19,7 +21,27 @@ if __name__ == "__main__":
     temp_message = input_container.text_input("Type your message here...")
 
     if input_container.button("Send"):
-        msgr.add(temp_message, True)
+        promptObject = {
+            "username": "John",
+            "prompt": temp_message
+        }
+        prompt = json.dumps(promptObject)
+        data = requests.post("http://localhost:8081/ai", prompt)
+
+        id = data.text
+        id = id.replace('"', '')
+
+        r = requests.get(f"http://localhost:8081/ai/{id}")
+
+        response_json = r.json()  # Parse the JSON response
+
+        message = response_json['prompt']
+
+        if data:
+            msgr.add(message, True)
+
+        else:
+            st.error("Error")
 
     # Display messages if they exist
     if "history" in st.session_state:
