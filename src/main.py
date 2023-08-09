198f30ced2c44b028be7bb31a65670c57abd71a2
Ghost <OverlyGenericAddress@pm.me>
2023-08-09 19:25:46 +0200

    API established
    The app now does a api call to fetch the conversation history from the server on startup.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,27 @@
 """
-Main file for running the chat app
-Run with streamlit run src\main.py
-
-https://pypi.org/project/streamlit-chat/
+Main file for running the chat app.
 """
+import os
 import streamlit as st
+from dotenv import load_dotenv
 import interface as msgr
 from interface import init
 
+# The main function
 if __name__ == "__main__":
-    # Initialize layout and containers
+    # Load environment variables
+    load_dotenv()
+    st.session_state.api_url = os.getenv("API_URL")
+    st.session_state.api_key = os.getenv("API_KEY")
+    st.session_state.api_conversation_id = os.getenv("TEST_CONVERSATION_ID")
+
+    # Initialize the app
     init.layout()
+    init.conversation()
     message_container = st.container()
     input_container = st.container()
 
-    # Handle user input
+    # Handle the user input
     temp_message = input_container.text_input("Type your message here...")
 
     if input_container.button("Send"):
