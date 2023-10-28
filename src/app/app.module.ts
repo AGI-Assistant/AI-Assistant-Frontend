474ddfdb98776d41d635656ad4e778438ce5c221
Ghost <OverlyGenericAddress@pm.me>
2023-10-28 23:03:12 +0200

    Refactored app, added new navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Navigation bar imports
+import { NavigationBarComponent } from './navbar/navigation-bar/navigation-bar.component';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+// Polling imports
+import { HttpClientModule } from '@angular/common/http';
+
+// Chat imports
+import { ChatComponent } from './chat/chat.component';
+import { MessageComponent } from './message/message.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+// Landing page imports
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    ChatComponent,
+    MessageComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatListModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PollingService } from '../polling/polling.service';
+import { Subscription } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
+// Message interface
+interface ChatResponse {
+  text: string,
+  isUser: boolean,
+  time: Date
+}
+
+// Component decorator
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit, OnDestroy {
+  constructor(private http: HttpClient, private pollingService: PollingService) { }
+
+  // Variable initialization
+  backendUrl: string = 'http://127.0.0.1:8000/api/'
+  newMessage: string = '';
+  messages: ChatResponse[] = [];
+  private pollingSubscription!: Subscription;
+
+  // Subscribes to the polling service, to recive new messages
+  ngOnInit(): void {
+    this.pollingSubscription = this.pollingService.infoPoll$.subscribe(
+      newInfo => {
+        this.checkForNewMessages(newInfo);
+      }
+    );
+  }
+
+  // Unsubscribes from the polling service on destroy
+  ngOnDestroy() {
+    if (this.pollingSubscription) {
+      this.pollingSubscription.unsubscribe();
+    }
+  }
+
+  // Check if the polling service update contains new messages
+  checkForNewMessages(newInfo: any): void {
+    if (newInfo && newInfo.messageID) {
+      const messageIDs = newInfo.messageID;
+      // Now messageIDs is an array of the new message IDs
+      // You can process these IDs as needed
+      // For example, you could fetch the full message data for these IDs from the server
+    }
+  }
+
+  // Sends a request to the server to get a message and add it to the message array
+  getMessage() {
+    const headers = new HttpHeaders().set('messageID', String(this.messages.length));
+    this.http.get<ChatResponse[]>(this.backendUrl + 'get/messages', { headers }).subscribe({
+      next: (response) => {
+        const newMessages = response.map(message => ({
+          ...message,
+          time: new Date(message.time)
+        }));
+        this.messages.push(...newMessages);
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
+  }
+
+  // Sends a message to the server
+  sendMessage() {
+    const currentTime = new Date();
+    const payload = { text: this.newMessage, isUser: true, time: currentTime };
+
+    this.http.post<ChatResponse>(this.backendUrl + 'chat', payload).subscribe({
+      next: (response) => {
+        this.messages.push({ text: response.text, isUser: false, time: new Date(response.time) });
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
+    this.messages.push(payload);
+    this.newMessage = '';
+  }
+
+  // Sends a request to the server to get the chat history
+  getChatHistory() {
+    this.http.get<ChatResponse[]>(this.backendUrl + 'get/messages')
+  }
+}
