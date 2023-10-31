137d6e918d245f02657a06787fd906eb53319053
Ghost <OverlyGenericAddress@pm.me>
2023-10-31 12:25:52 +0100

    Fully implemented chat logic and backend mockup for testing

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -20,7 +20,7 @@ interface MessageDict {
 })
 export class ChatComponent implements OnInit, OnDestroy {
   constructor(private http: HttpClient, private pollingService: PollingService, private navService: NavigationBarService) { 
-    this.getChatHistory('0');
+    this.getChatHistory('conversation0');
   }
   
   toggleNavbar() {
@@ -51,14 +51,14 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   // Check if the polling service update contains new messages
   checkForNewMessages(newInfo: any): void {
-    if (newInfo && newInfo.messageID) {
-      this.getMessages(newInfo.messageID);
+    if (newInfo && newInfo.conversationIds) {
+      //this.getMessages(newInfo.conversationIds);
     }
   }
 
-  // Sends a request to the server to get a message and add it to the message array
-  getMessages(messageIDs: string[]) {
-    const headers = new HttpHeaders().set('messageIDs', messageIDs);
+  // Sends a request to the server to get a message and adds it to the message array
+  getMessages(messageIds: string[]) {
+    const headers = new HttpHeaders().set('messageIds', messageIds);
     this.http.get<MessageDict[]>(this.backendUrl + 'get/messages', { headers }).subscribe({
       next: (response) => {
         const newMessages = response.map(message => ({
@@ -73,36 +73,38 @@ export class ChatComponent implements OnInit, OnDestroy {
     });
   }
 
-// Sends a message to the server
-postMessage() {
-  const timestamp = new Date();
-  this.http.post(this.backendUrl + 'post/message', { text: this.newMessage, isUser: true, time: Math.floor(timestamp.getTime() / 1000) }, { observe: 'response' }).subscribe({
-    next: (response) => {
-      if (response.status === 201) {
-        this.messages.push({ text: this.newMessage, isUser: true, time: timestamp });
-        this.newMessage = '';
-      } else {
-        console.error('Unexpected status code:', response.status);
+  // Sends a message to the server
+  postMessage() {
+    const timestamp = new Date();
+    this.http.post(this.backendUrl + 'post/message', { text: this.newMessage, isUser: true, time: Math.floor(timestamp.getTime() / 1000) }, { observe: 'response' }).subscribe({
+      next: (response) => {
+        if (response.status === 201) {
+          this.messages.push({ text: this.newMessage, isUser: true, time: timestamp });
+          this.newMessage = '';
+        } else {
+          console.error('Unexpected status code:', response.status);
+        }
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
       }
-    },
-    error: (error) => {
-      console.error('There was an error!', error);
-    }
-  });
-}
-
-
+    });
+  }
 
   // Sends a request to the server to get the chat history
-  getChatHistory(conversationID: string) {
-      const headers = new HttpHeaders().set('conversationID', conversationID);
-      this.http.get<MessageDict[]>(this.backendUrl + 'get/history', { headers }).subscribe({
+  getChatHistory(conversationId: string) {
+      const headers = new HttpHeaders().set('conversationId', conversationId);
+      this.http.get<any>(this.backendUrl + 'get/history', { headers }).subscribe({
         next: (response) => {
-          const newMessages = response.map(message => ({
-            ...message,
-            time: new Date(message.time)
-          }));
-          this.messages= newMessages;
+          if (response.hasOwnProperty(conversationId)) {
+            const newMessages = response[conversationId].map((message: MessageDict) => ({
+              ...message,
+              time: new Date(message.time)
+            }));
+            this.messages = newMessages;
+          } else {
+            console.warn(`No conversation found with ID: ${conversationId}`);
+          }
         },
         error: (error) => {
           console.error('There was an error!', error);
--- tests/backend_mockup.py
+++ tests/backend_mockup.py
@@ -1,45 +1,62 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Response, Header, Body
 from fastapi.responses import JSONResponse
 from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
+from typing import List, Dict
 
 # Define the app
 app = FastAPI()
 
-
 # CORS settings
 origins = [
-    "http://localhost:4200",
+  "http://localhost:4200",
 ]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
+# Mock chat history
+chat_history = {
+  "conversation0": [
+    {'text': 'Hello', 'isUser': False, 'time': int(datetime.now().timestamp())},
+    {'text': 'World', 'isUser': True, 'time': (int(datetime.now().timestamp()) + 1)},
+  ],
+  "conversation1": [
+    {'text': 'Hi', 'isUser': True, 'time': int(datetime.now().timestamp())},
+  ],
+}
+
+content = {'messageIds': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']}
+
+"""
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(f"Echo: {data}")
+"""
+
 
 @app.get('/api/get/polling')
 async def polling():
-   content = {'text': 'Hello World', 'isUser': False, 'time': int(datetime.now().timestamp())}
-   print('polling...')
-   return JSONResponse(content=content, status_code=204)
+    return Response(status_code=204)
 
-@app.get("/api/get/messages")
-async def read_chat(message: dict):
-    # Generate a response
-    answer = f"Your {i} mock answer..."  # Replace with your logic
-    i += 1
-    # Package and return the response
-    return {'text': answer, 'isUser': False, 'time': int(datetime.now().timestamp())}
 
-i = 0
+@app.post("/api/post/message")
+async def post_message(message: dict = Body(None)):
+    print(message)
+    return Response(status_code=201)
 
+
+@app.get("/api/get/history")
+async def get_chat_history(conversationId: str = Header(None)):
+    if conversationId is None:
+        return JSONResponse(content={"error": "Missing conversationId header"}, status_code=400)
+    history_content = chat_history.get(conversationId, [])
+    return JSONResponse(content={conversationId: history_content}, status_code=200)
