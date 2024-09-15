007b52279118f5b06ba860bb53a7fecd10a1134a
Ghost <OverlyGenericAddress@pm.me>
2024-09-15 20:52:20 +0200

    Started reconfiguring pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {}
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Assistant';
+
+  constructor(private statusService: StatusBarService) { }
+
+  toggleNavbar() {
+    this.statusService.toggleSidenav();
+  }
 }
---
+++ src/app/headbar/headbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './headbar.component.html',
+  styleUrl: './headbar.component.scss'
+})
+export class HeadbarComponent {
+
+}
